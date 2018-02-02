b45c2ac7c49037562c6972649d18df0c4354dcb1
Zach Smith <kerbonauts@gmail.com>
2018-02-02 16:20:35 -0700

    Renamed auto class files.

--- src/org/usfirst/frc/team2486/robot/Auto/Blue/RightSwitch.java
+++ src/org/usfirst/frc/team2486/robot/Auto/Blue/RightSwitch.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team2486.robot.Auto.Blue;
 
 import org.usfirst.frc.team2486.robot.Interfaces.AutoMode;
 
-public class CenterSwitchRight implements AutoMode
+public class RightSwitch implements AutoMode
 {
 	@Override
 	public void Run()
